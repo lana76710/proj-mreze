d9159b6776c83b073463d4e6ad1d49387b8823be
lana76710 <lana.gojkovic3@gmail.com>
2026-01-29 18:46:10 +0100

    Add files via upload

---
+++ Sistem za upravljanje datotekama1/Common/Datoteka.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common
+{
+    [Serializable]
+    public class Datoteka
+    {
+        public string NazivDatoteke { get; set; }
+        public string Autor { get; set; }
+        public string VremePoslednjePromene { get; set; }
+        public string Sadrzaj { get; set; }
+    }
+}
---
+++ Sistem za upravljanje datotekama1/Common/Zahtev.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Common
+{
+    public enum Operacija
+    {
+        Dodavanje,
+        Izmena,
+        Uklanjanje
+    }
+
+    [Serializable]
+    public class Zahtev
+    {
+        public string PutanjaDoDatoteke { get; set; }
+        public Operacija Operacija { get; set; }
+    }
+}
